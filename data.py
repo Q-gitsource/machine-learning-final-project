3c415003e82b754ee86b0b55ba12ce037b808980
Adrian Santos <asantos27@cornellcollege.edu>
2025-09-12 10:34:30 -0500

    implemented hot encoder for categorical columns and logistic model training

--- data.py
+++ data.py
@@ -1,6 +1,7 @@
 import os
 import kagglehub
 import pandas as pd
+from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import OneHotEncoder
 
@@ -11,14 +12,27 @@ csv_path = path + "/" + os.listdir(str(path))[0]
 
 housing_prices_dataset = pd.read_csv(csv_path)
 untrimmed_df = pd.DataFrame(housing_prices_dataset)
-movies_df = untrimmed_df[["title", "release_date", "revenue", "runtime", "vote_average", "adult", "budget", "original_language", "genres", "keywords"]]
-movies_df.dropna(inplace=True) # Drop rows missing values
+movies_df = untrimmed_df[["release_date", "revenue", "runtime", "vote_average", "adult", "budget", "original_language", "genres", "keywords"]]
+movies_df = movies_df.dropna()  # Drop rows missing values
+
+# Hot encoder for categorical columns
+
+encoder = OneHotEncoder()
+categorical = ['adult', 'original_language', 'genres', 'keywords']
+swapped = encoder.fit_transform(movies_df[categorical]).toarray()
+feature_names = encoder.get_feature_names_out(categorical)
+transdata = pd.DataFrame(swapped, columns= feature_names)
+FinalMovies = pd.concat([movies_df.drop(categorical, axis=1).reset_index(drop=True), transdata.reset_index(drop=True)], axis=1)
 
 # Separate features x and target y
-x = movies_df.drop("vote_average", axis=1)
-encoder = OneHotEncoder()
-x_encoded = encoder.fit_transform(x)
 
-y = movies_df["vote_average"]
+x = FinalMovies.drop(["vote_average"], axis=1)
 
-print(x_encoded)
+y = FinalMovies["vote_average"]
+
+# train test split on data
+
+x_tr, x_ts, y_tr, y_ts = train_test_split(x,y, test_size=0.8)
+
+log = LogisticRegression()
+log.fit(x_tr,y_tr)
